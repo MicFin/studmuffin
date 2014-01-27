f9a96add69da13900262c4de699965d5ccbf7fb5
Josh Prince <josh.prince@arcadiasolutions.com>
2014-01-26 19:36:07 -0500

    Initialize rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studmuffin::Application.config.secret_token = 'a956f325a51724a0092edce0993d9eba2357b9af7ff2468d159a6d05cd1f9c7ce21797aaa3b990bd47e0df5801a241d5f919d09dea33ba97bf12b81661ecc9ef'
