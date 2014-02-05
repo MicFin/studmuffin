bfe0b490ee087037c84e963dc02ac419416c143f
Josh Prince <josh.prince@arcadiasolutions.com>
2014-02-04 23:20:36 -0500

    Clean up User migration and remove password field from User#edit
    When upgrading from the last commit you will need to run the following tasks: rake kill_postgres_connections, rake db:reset, rake db:migrate, rake db:test:prepare, rake db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140129044521) do
+ActiveRecord::Schema.define(:version => 20140205035503) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "encrypted_password"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
 end
