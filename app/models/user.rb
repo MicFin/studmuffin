d6f2dc6cd598622c7a91c5471b39c952fa306822
Maxthreve <Maxthreve@gmail.com>
2014-03-05 20:51:05 -0500

    removing timeout changes from branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :fullname, :allergy_ids, :password_confirmation
-  devise :database_authenticatable, :registerable, :timeoutable
+  devise :database_authenticatable, :registerable
   has_many :users_allergies
   has_many :notes
   has_many :allergies, through: :users_allergies
