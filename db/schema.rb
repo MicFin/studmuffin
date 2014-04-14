45bb8470597ca25c005ae46ca09463e5acbc6dd1
Michael Finneran <mrfinneran@gmail.com>
2014-04-14 11:45:57 -0400

    added libv8 gem per installation request

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140223215052) do
+ActiveRecord::Schema.define(:version => 20140302215341) do
 
   create_table "allergies", :force => true do |t|
     t.string   "name"
@@ -28,15 +28,33 @@ ActiveRecord::Schema.define(:version => 20140223215052) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",              :default => "",    :null => false
-    t.string   "encrypted_password", :default => "",    :null => false
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,     :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.string   "fullname"
-    t.boolean  "admin",              :default => false, :null => false
+    t.boolean  "admin",                  :default => false, :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "users_allergies", :id => false, :force => true do |t|
     t.integer  "allergy_id"
