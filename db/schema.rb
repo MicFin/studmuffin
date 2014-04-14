f66c5705011bf5c9da5ac23c763c75394f002136
Michael Finneran <mrfinneran@gmail.com>
2014-04-14 16:26:35 -0400

    remove userallergiesjointable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140414202346) do
+ActiveRecord::Schema.define(:version => 20140414202511) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -43,12 +43,5 @@ ActiveRecord::Schema.define(:version => 20140414202346) do
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "users_allergies", :id => false, :force => true do |t|
-    t.integer  "allergy_id"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
