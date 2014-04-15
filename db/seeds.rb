b7c12d9df79c32475152d5feece4f4779bea8414
Michael Finneran <mrfinneran@gmail.com>
2014-04-15 14:11:28 -0400

    removed allergy seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,3 @@ for i in 1..3
   name = "User #{i}"
   User.find_or_create_by_email email: email, password: 'changeme', fullname: name
 end
-Allergy.create(name: 'Wheat')
-Allergy.create(name: 'Soy')
-Allergy.create(name: 'Dairy')
-Allergy.create(name: 'Egg')
-Allergy.create(name: 'Peanut/Treenut')
-Allergy.create(name: 'Fish/Shellfish')
