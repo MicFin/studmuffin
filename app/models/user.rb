f43e01d61c15b4eaf6fcf47f446e09d5573b53eb
Michael Finneran <mrfinneran@gmail.com>
2014-04-17 19:01:21 -0400

    added confirmable password and validatable email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :admin, :phone_number, :sign_in_count, :admin, :encrypted_password, :created_at, :updated_at, :last_sign_in_at, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_ip 
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :admin, :phone_number, :sign_in_count, :admin, :encrypted_password, :created_at, :updated_at, :last_sign_in_at, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :reset_password_token, :reset_password_sent_at, :remember_created_at
 
-  devise :database_authenticatable, :registerable, :timeoutable, :trackable
+  devise :database_authenticatable, :registerable, :timeoutable, :trackable, :recoverable, :rememberable, :validatable, :timeoutable
 
   validates_presence_of :first_name, :last_name, :email, :password, {message: "can't be blank" }
-  validates :email, :uniqueness => true
+  # validates :email, :uniqueness => true
   validates_presence_of :phone_number, {message: "can't be blank.  Your phone number will ONLY be used in case of a connection problem.  Otherwise we will NEVER call you.  And we will NEVER share your number." }
 
   def is_admin?
