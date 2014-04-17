5f3f71f082921117cad15eed24f5ae7f732afcfa
Michael Finneran <mrfinneran@gmail.com>
2014-04-17 15:55:04 -0400

    added phone_number field to view and to attr_accessor in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :admin
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :admin, :phone_number 
   devise :database_authenticatable, :registerable, :timeoutable
 
   validates_presence_of :first_name, :last_name, :email, :password, {message: "can't be blank" }
   validates :email, :uniqueness => true
-
+  validates_presence_of :phone_number, {message: "can't be blank.  Your phone number will only be used in case of a connection problem.  Otherwise we will NEVER call you.  And we will NEVER share your number." }
 
   def is_admin?
     if self.admin == true
