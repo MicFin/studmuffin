88283c01e971e76eaaae460a6e507aefff3b9e30
Michael Finneran <mrfinneran@gmail.com>
2014-04-18 00:30:59 -0400

    added phone number validation and formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :timeoutable, :trackable, :recoverable, :rememberable, :validatable, :timeoutable
 
   validates_presence_of :first_name, :last_name, {message: "can't be blank" }
+  # validates :phone_number, format: { with: 1?\s*\W?\s*([2-9][0-8][0-9])\s*\W?\s*([2-9][0-9]{2})\s*\W?\s*([0-9]{4})(\se?x?t?(\d*))?;, message: "bad format" }
+  phony_normalize :phone_number, :default_country_code => 'US'
   # validates :email, :uniqueness => true
-  validates_presence_of :phone_number, {message: "can't be blank.  Your phone number will ONLY be used in case of a connection problem.  Otherwise we will NEVER call you.  And we will NEVER share your number." }
+  validates_presence_of :phone_number, {message: "is an invalid number.  Your phone number will ONLY be used in case of a connection problem.  Otherwise we will NEVER call you.  And we will NEVER share your number." }
+  validates :phone_number, :phony_plausible => true
 
   def is_admin?
     if self.admin == true
