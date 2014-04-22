984c6010cceef415546e78f5a2de21f09b18f2fc
Michael Finneran <mrfinneran@gmail.com>
2014-04-22 13:00:25 -0400

    added families controller

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,38 @@
+class FamiliesController < ApplicationController
+  def index
+    @families = user.all
+  end
+
+  def show
+    @family = Family.find(params[:id])
+    @family_dietary_restrictions = @family.dietary_restrictions
+    @family_members = @family.users 
+  end
+
+  def new
+    @family = Family.new
+  end
+
+  def edit
+    @family = Family.find(params[:id])
+  end
+
+  def create
+    @family = Family.new(params[:family])
+    @family.save
+    redirect_to root_path
+  end
+
+  def update
+    @family = Family.find(params[:id])
+    @family.update_attributes(params[:family])
+    @family.save
+    redirect_to root_path(anchor: 'profile')
+  end
+
+  def destroy
+    @family = Family.find(params[:id])
+    @family.destroy
+    redirect_to families_path
+  end
+end
--- app/models/family.rb
+++ app/models/family.rb
@@ -3,4 +3,17 @@ class Family < ActiveRecord::Base
   attr_accessible :location, :name
   has_many :user_families
   has_many :users, through: :user_families
+
+  def dietary_restrictions
+    family_restrictions = []
+    self.users.each do |family_member|
+      if family_member.dietary_restrictions.count >= 1 
+        family_member.dietary_restrictions.each do |dietary_restriction|
+          family_restrictions << dietary_restriction
+        end
+      end
+    end
+    return family_restrictions.uniq
+  end
+
 end
