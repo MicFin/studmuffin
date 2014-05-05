7a0f90115c729d98ed6ab3bd3acb4dff64218d76
Michael Finneran <mrfinneran@gmail.com>
2014-05-05 12:15:20 -0400

    merge

--- app/assets/javascripts/family_page.js
+++ app/assets/javascripts/family_page.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
 	
-<<<<<<< HEAD
-	for (var i=1; i <= 10; i++) {
-		$("#child-forms-div-"+i).hide()
-	};
-
-	var family_clicks = 10;
-	$("#add-family-button").click(function(){
-			var last_form = family_clicks + 1
-			$("#child-forms-div-"+family_clicks).show();
-			$("#child-forms-div-0").addClass("member-form-white");
-			$("#child-forms-div-"+last_form).addClass("member-form-white");
-			family_clicks = family_clicks - 1;
-	});
-
-
-	// $(".date-picker").datepicker();
-=======
 	// for (var i=1; i <= 10; i++) {
 	// 	$("#child-forms-div-"+i).hide()
 	// };
@@ -47,7 +30,6 @@ $(document).ready(function() {
 		$("#start-with-you").hide();
 		$("#carousel-back-button").addClass("orange-background");
 	});
->>>>>>> cocoon
 
 	for (var i=1; i <= 100; i++) {
 		// set popovers
@@ -57,12 +39,6 @@ $(document).ready(function() {
 		// hide other allergy/disease field until clicked
 
 	};
-<<<<<<< HEAD
-  	// $("#intolerance-1-checkbox").click(function() {
-   //  	$("#intolerance-1-field")[this.checked ? "show" : "hide"]();
-
-  	// });
-=======
 
 // ATTEMPTS AT HIDING THEN SHOWING INPUT DIV
  //  	// $("#intolerance-1-checkbox").click(function() {
@@ -89,7 +65,6 @@ $(document).ready(function() {
 	// // };
 
 
->>>>>>> cocoon
 
 	$(".restrictions-div").hide();
  	$(".create-diets-button").hide(); 
