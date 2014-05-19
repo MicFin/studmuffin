dab6641a583fdc7d3c7c72a8bc35896c0c2dcd70
Michael Finneran <mrfinneran@gmail.com>
2014-05-19 18:06:35 -0400

    migrated dietitian

--- app/models/dietitian.rb
+++ app/models/dietitian.rb
@@ -2,9 +2,9 @@ class Dietitian < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name 
   # attr_accessible :title, :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140519194450) do
+ActiveRecord::Schema.define(:version => 20140519220106) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -66,6 +66,35 @@ ActiveRecord::Schema.define(:version => 20140519194450) do
     t.integer  "order"
     t.text     "description"
   end
+
+  create_table "dietitians", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        :default => 0,  :null => false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "dietitians", ["confirmation_token"], :name => "index_dietitians_on_confirmation_token", :unique => true
+  add_index "dietitians", ["email"], :name => "index_dietitians_on_email", :unique => true
+  add_index "dietitians", ["reset_password_token"], :name => "index_dietitians_on_reset_password_token", :unique => true
+  add_index "dietitians", ["unlock_token"], :name => "index_dietitians_on_unlock_token", :unique => true
 
   create_table "families", :force => true do |t|
     t.string   "name"
