69818a707ce87b1152739e065b9f0a68c01bf715
Michael Finneran <mrfinneran@gmail.com>
2014-06-01 23:05:39 -0400

    added room controller, model, and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140519220106) do
+ActiveRecord::Schema.define(:version => 20140602030249) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -128,6 +128,14 @@ ActiveRecord::Schema.define(:version => 20140519220106) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
+
+  create_table "rooms", :force => true do |t|
+    t.string   "name"
+    t.string   "sessionId"
+    t.boolean  "public"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
